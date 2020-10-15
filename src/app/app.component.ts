5863ba9b03e75db8471cd55953435689e078d2df
Bharathi S <TEM200000104@mobiusservices.in>
2020-10-15 08:51:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: '[app-root]',
+  templateUrl:  './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-app';
+  values = '';
+  clickMessage =" ";
+  onClickMe(){
+    this.clickMessage = 'You are my hero'
+  }
+  onKey(event: any) { // without type info
+    this.values += (event.target as HTMLInputElement).value + ' || ';
+  }
+ 
 }
